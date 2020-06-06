a4cc65593b5f66b901bd30c03b84e3cbb52f295a
WhiteGrouse <39170892+WhiteGrouse@users.noreply.github.com>
2020-06-06 15:01:02 +0900

    clear

--- examples/ListenExample/src/FlowyExamples/ListenExample/Main.php
+++ examples/ListenExample/src/FlowyExamples/ListenExample/Main.php
@@ -5,14 +5,12 @@ use flowy\Flowy;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\plugin\PluginBase;
 use function flowy\{listen, start};
-use flowy\standard\Standard;
 
 class Main extends PluginBase
 {
     function onEnable()
     {
         Flowy::bootstrap();
-        Standard::bootstrap();
         $stream = start($this);
         $stream->run(function($stream) {
             while (true) {
